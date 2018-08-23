f4314a7b16f9d747bd8249f53bb561c4bd94c751
Sebastian Maddox <sebastianknightmaddox@gmail.com>
2018-08-24 11:49:04 +1200

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		cssmin: {
+			target: {
+				files: [{
+					expand: true,
+					cwd: 'public/css/',
+					src: ['*.css', '!*.min.css'],
+					dest: 'public/css/',
+					ext: '.min.css'
+				}]
+			}
+		},
+		jshint: {
+			all: ["public/js/script.js"]
+		},
+		uglify: {
+			all: {
+				files: {
+					"public/js/script.min.js": ["public/js/script.js"]
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: ["public/css/style.css"],
+				tasks: ["csslint", "cssmin"]
+			},
+			js: {
+				files: ["public/js/script.js"],
+				tasks: ["jshint", "uglify"]
+			}
+		}
+	});
+
+	// grunt.loadNpmTasks();
+	grunt.loadNpmTasks("grunt-contrib-cssmin");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+
+	// grunt.registerTask();
+};
