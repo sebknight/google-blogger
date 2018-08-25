0a7186403892c44713f1d295fe65180974aaee67
sebknight <37192621+sebknight@users.noreply.github.com>
2018-08-25 19:55:40 +1200

    added google api

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const path = require("path");
 const config = require("./config");
+const {google} = require("googleapis");
+const blogger = google.blogger({
+	version: 'v3',
+	auth: config.bloggerKey
+});
+const params = {
+	blogID: 4236176886935208807
+};
 
 const app = express();
 
-app.use(function(req,res,next){
+app.use(function(req, res, next){
 	console.log(`${req.method} request for ${req.url}`);
 	next();
 });
@@ -15,7 +23,7 @@ app.use(`/bootstrap`, express.static(path.join(__dirname, `node_modules/bootstra
 app.use(`/jquery`, express.static(path.join(__dirname, `node_modules/jquery/dist/jquery.min.js`)));
 
 
-app.get(`/`, (req,res) => res.sendFile(`${__dirname}/public/index.html`));
+app.get(`/`, (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 // app.get(`/`, function(req,res){
 // 	res.sendFile(`${__dirname}/public/index.html`);
 // });
@@ -23,3 +31,12 @@ app.get(`/`, (req,res) => res.sendFile(`${__dirname}/public/index.html`));
 app.set(`port`, (process.env.PORT || 3000));
 
 app.listen(app.get(`port`), () => console.log(`Server is running on port ${app.get(`port`)}`));
+
+blogger.blogs.get(params, (err, res) => {
+	if (err) {
+		console.error(err);
+		throw err;
+	}
+	console.log((`The blog url is ${res.data.url}`));
+	
+});
